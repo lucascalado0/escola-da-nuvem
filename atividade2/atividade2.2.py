4d70af130e22da0867eed18af116c83737e0a43a
devlucascalado <devlucascalado@gmail.com>
2025-10-28 20:31:33 -0300

    atividade 2

--- atividade2/atividade2.2.py
+++ atividade2/atividade2.2.py
@@ -9,3 +9,14 @@ Porcentagem de desconto: 20%
 O programa deve calcular o valor do desconto e o preço final, exibindo todos os detalhes.
 """
 
+nomeProduto = "Camiseta"
+precoOriginal = 50.00
+desconto = 0.2
+
+precoFinal = precoOriginal - (precoOriginal * desconto) 
+
+print("---Descrição---")
+print("Produto: " + nomeProduto)
+print(f"Preço original: R${precoOriginal}")
+print(f"Preço com desconto de 20%: R${precoFinal}")
+
