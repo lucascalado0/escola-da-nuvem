26f0b51f4544aedc3cec7db0474492a994987245
devlucascalado <devlucascalado@gmail.com>
2025-11-02 21:51:08 -0300

    atividade 3

---
+++ atividade3/atividade3.5.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que determine se um ano inserido pelo usuário é bissexto ou não.
+Um ano é bissexto se for divisível por 4, exceto anos centenários (divisíveis por 100)
+que não são divisíveis por 400. em python
+"""
+
+ano = int(input("Informe um ano: "))
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(f"{ano} é um ano bissexto.")
+else:
+    print(f"{ano} não é um ano bissexto.")
